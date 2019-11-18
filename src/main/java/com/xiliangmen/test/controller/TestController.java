aa0cb6d115f9556704b7a43722c7bfc3384ef211
belongfeng <belongfeng@gmail.com>
2019-11-18 13:40:01 +0800

    controller

---
+++ src/main/java/com/xiliangmen/test/controller/TestController.java
@@ -0,0 +1,10 @@
+package com.xiliangmen.test.controller;
+
+/**
+ * @Classname TestController
+ * @Description TODO
+ * @Date 2019/11/18 13:37
+ * @Created by FengYu
+ */
+public class TestController {
+}
---
+++ src/main/java/com/xiliangmen/test/service/TestService.java
@@ -0,0 +1,10 @@
+package com.xiliangmen.test.service;
+
+/**
+ * @Classname TestService
+ * @Description TODO
+ * @Date 2019/11/18 13:39
+ * @Created by FengYu
+ */
+public class TestService {
+}
